5691e3d76cf6683e9b785fe08e83a6e488a49ca4
Hong Yeong Ju <abc123472000@naver.com>
2018-05-26 14:16:49 +0900

    액티비티 수정중

--- app/src/main/java/com/midasit/challenge/ui/menu/AdminMenuDetailActivity.java
+++ app/src/main/java/com/midasit/challenge/ui/menu/AdminMenuDetailActivity.java
@@ -1,13 +1,24 @@
 package com.midasit.challenge.ui.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.midasit.challenge.R;
 
-public class AdminMenuDetailActivity extends AppCompatActivity {
+public class AdminMenuDetailActivity extends AppCompatActivity implements Button.OnClickListener{
+
+    TextView name;
+    TextView des;
+    ImageView img;
+    TextView price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +31,31 @@ public class AdminMenuDetailActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("메뉴 상세화면");
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Button delete_button = (Button)findViewById(R.id.delete_btn);
+        delete_button.setOnClickListener(this);
+        Button modifi_button = (Button)findViewById(R.id.modification_btn);
+        modifi_button.setOnClickListener(this);
+
+
+        name = (TextView)findViewById(R.id.name_tv);
+        des = (TextView)findViewById(R.id.description_cafe_text);
+        img = (ImageView)findViewById(R.id.image_img);
+
+
     }
 
 
-
-
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.delete_btn:
+                Toast.makeText(getApplicationContext(), "삭제", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.modification_btn:
+                Intent intent = new Intent(getApplicationContext(), AdminMenuDetailModifiActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
